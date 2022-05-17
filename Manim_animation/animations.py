a0e175e00363e94fb87f1db3587c9893d3e4cce1
RahimFatih <dfirat998@gmail.com>
2022-05-17 21:59:56 +0200

    progress with heart

--- Manim_animation/animations.py
+++ Manim_animation/animations.py
@@ -92,5 +92,63 @@ class PlotParametricFunction(Scene):
 
     def construct(self):
         func = ParametricFunction(self.func, t_range = np.array([0, 7]), fill_opacity=0).set_color(RED)
-        self.add(func.scale(0.1))
-	
+        self.play(Create(func),run_time=4)
+from manim import *
+
+from manim import *
+
+class heart(MovingCameraScene):
+    def func(self, t):
+        return np.array(((16*(np.sin(t))**3)/2,(13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t))/2+2, 0))
+
+    def construct(self):
+        self.camera.frame.save_state()
+
+        curve = ParametricFunction(lambda t: np.array([
+            (16*(np.sin(t))**3)/2,
+            (13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t))/2+2, 0
+            ]),color=PINK,t_range = np.array([0, 2*PI]))
+        plane = number_plane = NumberPlane(
+            x_range=[-10, 10, 1],
+            y_range=[-10, 10, 1],
+            background_line_style={
+                "stroke_color": TEAL,
+                "stroke_width": 4,
+                "stroke_opacity": 0.6
+            }
+        )
+        plane.center()
+        line_graph = plane.plot_line_graph(
+            x_values = [0, 1.5, 2, 2.8, 4, 6.25],
+            y_values = [1, 3, 2.25, 4, 2.5, 1.75],
+            line_color=GOLD_E,
+            vertex_dot_style=dict(stroke_width=3,  fill_color=PURPLE),
+            stroke_width = 4,
+        )
+        vg=VGroup(plane,curve)
+        bool_ops_text = MarkupText("<u>Boolean Operation</u>").next_to(curve, UP * 3)
+        #self.add(plane)
+        self.play(Create(bool_ops_text),run_time=1)
+        self.play(Create(vg),self.camera.auto_zoom(curve),run_time=4)
+        self.wait(3)
+
+
+from manim import *
+
+class DiscontinuousExample(Scene):
+    def construct(self):
+        ax1 = NumberPlane((-3, 3), (-4, 4))
+        ax2 = NumberPlane((-3, 3), (-4, 4))
+        VGroup(ax1, ax2).arrange()
+        discontinuous_function = ParametricFunction(lambda t: np.array([
+            (16*(np.sin(t))**3)/2,
+            (13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t))/2+2, 0
+            ]),color=PINK,t_range = np.array([0, 2*PI]))
+        incorrect = ax1.plot(discontinuous_function, color=RED)
+        correct = ax2.plot(
+            discontinuous_function,
+            discontinuities=[-2, 2],  # discontinuous points
+            dt=0.1,  # left and right tolerance of discontinuity
+            color=GREEN,
+        )
+        self.add(ax1, ax2, incorrect, correct)
