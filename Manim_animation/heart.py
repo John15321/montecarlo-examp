ba1f14fbe79f404d1dc30b21ed03163ccbe02941
RahimFatih <dfirat998@gmail.com>
2022-05-22 18:27:31 +0200

    Animations put to gitignore

---
+++ Manim_animation/heart.py
@@ -0,0 +1,57 @@
+from turtle import dot
+
+from manim import *
+n=1000
+box=18
+rand_intX = np.random.randint(-500000,500000,(n))
+rand_intY = np.random.randint(-500000,500000,(n))
+
+rand_intX=rand_intX*(box/1000000)
+rand_intY=rand_intY*(box/1000000)-2
+t=np.pi/2-np.arctan2(rand_intX,rand_intY)
+
+heartX=(16*(np.sin(t))**3)/2
+heartY=(13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t))/2-2
+
+randXY_mod=np.sqrt(np.power(rand_intX,2)+np.power(rand_intY,2))
+heartXY_mod=np.sqrt(np.power(heartX,2)+np.power(heartY,2))
+dotcolor =(randXY_mod<heartXY_mod)
+
+class heart(MovingCameraScene):
+
+
+    def construct(self):        
+        self.add(NumberPlane())
+        curve = ParametricFunction(lambda t: np.array([
+            (16*(np.sin(t))**3)/2,
+            (13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t))/2-2, 0
+            ]),color=PINK,t_range = np.array([-PI, PI]))
+        
+        square=Square(box)
+        
+        ax = Axes(
+            x_range=[-12, 12, 1],
+            y_range=[-12, 12, 1],
+            tips=False,
+            axis_config={"include_numbers": True},
+        )
+        self.add(ax)
+        dots=VGroup()
+        for i in range(n):
+            dots+=(Dot(point=[rand_intX[i],rand_intY[i],0],color=('Green' if dotcolor[i] else 'Red')))
+        
+        self.add(square)
+        #self.play(self.camera.auto_zoom(square),run_time=1)
+        myTitle = MarkupText("<u>Pole serca</u>").to_corner(UP+LEFT)
+        inHeart_counter=MarkupText("Test=2").to_corner(UP+RIGHT)
+        outHeart_counter=MarkupText("Test=2").next_to(inHeart_counter,DOWN)
+        self.add(outHeart_counter)
+        self.add(inHeart_counter)
+        self.play(FadeIn(myTitle),run_time=1)
+        self.play(Create(curve),run_time=3)
+        self.play(Create(dots),run_time=3)
+        self.wait(3)
+
+
+
+
